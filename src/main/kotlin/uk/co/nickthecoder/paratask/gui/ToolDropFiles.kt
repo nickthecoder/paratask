7617f161b98e0263f969947d189706ec91e6b11b
Nick Robinson <nickthecoder@gmail.com>
2017-06-25 19:08:07 +0100

    Can now drop files onto PlacesTool

---
+++ src/main/kotlin/uk/co/nickthecoder/paratask/gui/ToolDropFiles.kt
@@ -0,0 +1,113 @@
+package uk.co.nickthecoder.paratask.gui
+
+import javafx.css.Styleable
+import javafx.scene.Node
+import javafx.scene.control.TableView
+import javafx.scene.input.DragEvent
+import javafx.scene.input.TransferMode
+import uk.co.nickthecoder.paratask.project.ToolPane
+import uk.co.nickthecoder.paratask.table.AbstractTableTool
+import uk.co.nickthecoder.paratask.table.WrappedRow
+import uk.co.nickthecoder.paratask.util.FileOperations
+import java.io.File
+
+/**
+ * A helper class for tools which can be a drop target for files.
+ * The files can either be dropped onto the tab, or onto the table
+ * R is the type or Row
+ */
+abstract class ToolDropFiles<R : Any>(
+        val tool: AbstractTableTool<R>,
+        val modes: Array<TransferMode> = TransferMode.ANY,
+        val rowModes: Array<TransferMode> = modes
+) {
+
+    var dropFilesOnTab: DropFiles? = null
+
+    var tableDropFiles: TableDropFiles? = null
+
+    var table: TableView<WrappedRow<R>>? = null
+        set(v) {
+            v?.let {
+                tableDropFiles = TableDropFiles(v, modes = rowModes) { event ->
+                    droppedFilesOnTable(event)
+                }
+            }
+        }
+
+    fun attached(toolPane: ToolPane) {
+        val halfTab = toolPane.halfTab
+        if (halfTab.isLeft()) {
+            dropFilesOnTab = DropFiles(halfTab.projectTab as Node, modes = modes) { event ->
+                droppedFilesOnNonRow(event.dragboard.files, event.transferMode)
+            }
+        }
+    }
+
+    fun detaching() {
+        dropFilesOnTab?.let {
+            it.cancel()
+            dropFilesOnTab = null
+        }
+    }
+
+    fun droppedFilesOnTable(event: DragEvent): Boolean {
+
+        val (row, _) = tool.findTableRow(event)
+        if (row != null && acceptDropOnRow(row)) {
+            return droppedFilesOnRow(row, event.dragboard.files, event.transferMode)
+        }
+        if (acceptDropOnNonRow()) {
+            return droppedFilesOnNonRow(event.dragboard.files, event.transferMode)
+        }
+        return false
+    }
+
+    abstract fun acceptDropOnRow(row: R): Boolean
+
+    abstract fun droppedFilesOnRow(row: R, files: List<File>, transferMode: TransferMode): Boolean
+
+    abstract fun droppedFilesOnNonRow(files: List<File>, transferMode: TransferMode): Boolean
+
+    open fun acceptDropOnNonRow() = true
+
+    fun fileOperation(dest: File, files: List<File>, transferMode: TransferMode): Boolean {
+
+        when (transferMode) {
+            TransferMode.COPY -> {
+                FileOperations.instance.copyFiles(files, dest)
+                return true
+            }
+            TransferMode.MOVE -> {
+                FileOperations.instance.moveFiles(files, dest)
+            }
+            TransferMode.LINK -> {
+                FileOperations.instance.linkFiles(files, dest)
+            }
+        }
+        return false
+    }
+
+    /**
+     * Style a ROW when dragging files to a directory, otherwise, style the table as a whole
+     */
+    inner class TableDropFiles(target: Node,
+                               source: Node = target,
+                               modes: Array<TransferMode> = TransferMode.ANY,
+                               dropped: (DragEvent) -> Boolean)
+        : DropFiles(target, source, modes, dropped) {
+
+        override fun styleableNode(event: DragEvent): Styleable? {
+            val (row, tableRow) = tool.findTableRow(event)
+            if (row != null && acceptDropOnRow(row)) {
+                return tableRow
+            }
+
+            if (acceptDropOnNonRow()) {
+                return super.styleableNode(event)
+            }
+
+            return null
+        }
+    }
+}
--- src/main/kotlin/uk/co/nickthecoder/paratask/tools/places/PlacesTool.kt
+++ src/main/kotlin/uk/co/nickthecoder/paratask/tools/places/PlacesTool.kt
@@ -19,15 +19,18 @@ package uk.co.nickthecoder.paratask.tools.places
 
 import javafx.scene.control.OverrunStyle
 import javafx.scene.image.ImageView
+import javafx.scene.input.TransferMode
 import uk.co.nickthecoder.paratask.TaskDescription
 import uk.co.nickthecoder.paratask.TaskParser
+import uk.co.nickthecoder.paratask.gui.ToolDropFiles
 import uk.co.nickthecoder.paratask.parameters.FileParameter
+import uk.co.nickthecoder.paratask.project.ToolPane
 import uk.co.nickthecoder.paratask.table.AbstractTableTool
 import uk.co.nickthecoder.paratask.table.Column
+import uk.co.nickthecoder.paratask.table.TableResults
 import uk.co.nickthecoder.paratask.table.TruncatedStringColumn
-import uk.co.nickthecoder.paratask.util.AutoRefreshTool
-import uk.co.nickthecoder.paratask.util.child
-import uk.co.nickthecoder.paratask.util.homeDirectory
+import uk.co.nickthecoder.paratask.util.*
+import java.io.File
 
 class PlacesTool : AbstractTableTool<Place>(), AutoRefreshTool {
 
@@ -36,6 +39,27 @@ class PlacesTool : AbstractTableTool<Place>(), AutoRefreshTool {
     val fileP = FileParameter("file", value = homeDirectory.child(".config", "gtk-3.0", "bookmarks"))
 
     lateinit var placesFile: PlacesFile
+
+    var dropFiles: ToolDropFiles<Place> = object : ToolDropFiles<Place>(this, modes = arrayOf(TransferMode.LINK), rowModes = TransferMode.ANY) {
+
+        override fun acceptDropOnRow(row: Place) = row.isDirectory()
+
+        override fun droppedFilesOnRow(row: Place, files: List<File>, transferMode: TransferMode): Boolean {
+            if (row.isFile()) {
+                return fileOperation(row.file!!, files, transferMode)
+            }
+            return false
+        }
+
+        override fun droppedFilesOnNonRow(files: List<File>, transferMode: TransferMode): Boolean {
+            for (file in files) {
+                placesFile.places.add(Place(placesFile, Resource(file), file.name))
+            }
+            placesFile.save()
+            return true
+        }
+
+    }
 
     init {
         taskD.addParameters(fileP)
@@ -48,15 +72,29 @@ class PlacesTool : AbstractTableTool<Place>(), AutoRefreshTool {
         columns.add(Column<Place, String>("location") { it.resource.path })
     }
 
+    override fun createTableResults(): TableResults<Place> {
+        val tableResults = super.createTableResults()
+
+        dropFiles.table = tableResults.tableView
+        return tableResults
+    }
+
     override fun run() {
         placesFile = PlacesFile(fileP.value!!)
         list = placesFile.places
         watch(fileP.value!!)
     }
 
+    override fun attached(toolPane: ToolPane) {
+        super.attached(toolPane)
+        dropFiles.attached(toolPane)
+    }
+
+
     override fun detaching() {
         super<AutoRefreshTool>.detaching()
         super<AbstractTableTool>.detaching()
+        dropFiles.detaching()
     }
 
     fun taskNew() = placesFile.taskNew()
