7474df5c7f7b503c6ffcf873489e712940ce1361
Nick Robinson <nickthecoder@gmail.com>
2017-07-30 13:30:36 +0100

    Save CompoundParameters in a more readable json format.

--- src/main/kotlin/uk/co/nickthecoder/paratask/util/JsonHelper.kt
+++ src/main/kotlin/uk/co/nickthecoder/paratask/util/JsonHelper.kt
@@ -3,13 +3,11 @@ package uk.co.nickthecoder.paratask.util
 import com.eclipsesource.json.JsonArray
 import com.eclipsesource.json.JsonObject
 import uk.co.nickthecoder.paratask.Task
-import uk.co.nickthecoder.paratask.parameters.GroupParameter
-import uk.co.nickthecoder.paratask.parameters.MultipleParameter
-import uk.co.nickthecoder.paratask.parameters.ValueParameter
+import uk.co.nickthecoder.paratask.parameters.*
 
 object JsonHelper {
 
-    fun parametersAsJsonArray(group: GroupParameter): JsonArray {
+    fun parametersAsJsonArray(group: AbstractGroupParameter): JsonArray {
         val jparameters = JsonArray()
         for (parameter in group.descendants()) {
             if (parameter is ValueParameter<*>) {
@@ -22,25 +20,31 @@ object JsonHelper {
         return jparameters
     }
 
-    private fun saveValue(parameter: ValueParameter<*>, jparameter: JsonObject) {
-        if (parameter is MultipleParameter<*>) {
-            val jvalues = JsonArray()
-            jparameter.set("values", jvalues)
-            parameter.innerParameters.forEach { inner ->
-                jvalues.add(inner.stringValue)
-            }
-        } else {
-            jparameter.set("value", parameter.stringValue)
-        }
-
-    }
-
     fun parametersAsJsonArray(task: Task): JsonArray {
         return parametersAsJsonArray(task.taskD.root)
     }
 
-    fun read(jparameters: JsonArray, group: GroupParameter) {
+    private fun saveValue(parameter: ValueParameter<*>, jparameter: JsonObject) {
+        if (parameter is MultipleParameter<*>) {
+            val jvalues = JsonArray()
+            jparameter.set("values", jvalues)
+
+            parameter.innerParameters.forEach { inner ->
+                if (inner is CompoundParameter) {
+                    val jArray = parametersAsJsonArray(inner)
+                    jvalues.add(jArray)
+                } else {
+                    jvalues.add(inner.stringValue)
+                }
+            }
+        } else {
+            jparameter.set("value", parameter.stringValue)
+        }
+    }
+
+    fun read(jparameters: JsonArray, group: AbstractGroupParameter) {
         for (jitem in jparameters.asArray()) {
+
             val ji = jitem.asObject()
             val name = ji.getString("name", null)
             if (name != null) {
@@ -52,7 +56,15 @@ object JsonHelper {
                         if (jvalues != null) {
                             val jvaluesArray = jvalues.asArray()
                             for (jvalue in jvaluesArray) {
-                                parameter.newValue().stringValue = jvalue.asString()
+                                val newValue = parameter.newValue()
+                                if (newValue is CompoundParameter) {
+                                    if (jvalue.isArray) {
+                                        val jvalueArray = jvalue.asArray()
+                                        read(jvalueArray, newValue)
+                                        continue
+                                    }
+                                }
+                                newValue.stringValue = jvalue.asString()
                             }
                         }
                         continue
@@ -65,7 +77,9 @@ object JsonHelper {
                 }
             }
         }
+
     }
+
 
     fun read(jparameters: JsonArray, task: Task) {
         read(jparameters, task.taskD.root)
